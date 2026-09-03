fc1ae4d1074836a1cca953cee0d8d44dab4c3648
Ashif-khan277 <ashifkhan6815@gmail.com>
2026-09-03 19:05:47 +0530

    Update urls for media serving

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,14 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     # ബ്ലോഗ് ആപ്പിലെ urls ലിങ്ക് ചെയ്യുന്നു
     path("", include("blog.urls")),
+    # പ്രൊഡക്ഷനിലും മീഡിയ ഫയലുകൾ കാണിക്കാൻ
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
 ]
-
-# മീഡിയ ഫയലുകൾ ലോക്കലായി ലോഡ് ചെയ്യാൻ ഇത് ചേർക്കുന്നു
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
